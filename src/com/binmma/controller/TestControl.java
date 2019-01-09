739f6986c59bfce6e1c352a952107a262e49a037
binmaa <m_bingo@qq.com>
2019-01-09 22:48:25 +0800

    [*]springmvc 请求处理流程

--- src/com/binmma/controller/TestControl.java
+++ src/com/binmma/controller/TestControl.java
@@ -1,5 +1,26 @@
 package com.binmma.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.binmma.model.User;
+
+@Controller
 public class TestControl {
+	@Autowired
+	@Qualifier("user")
+	private User user;
+	@RequestMapping(value="/testcontrol",method=RequestMethod.GET)
+	public ModelAndView testControl(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("msg", user.getName());
+		modelAndView.setViewName("MyJsp");
+		return modelAndView;
+	}
+	
 
 }
